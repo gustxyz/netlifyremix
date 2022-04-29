fad50e1e32d25958f507de354baf90a3443a71ce
Alexander M Long <alexanderlcodes@gmail.com>
2022-04-29 11:49:53 -0700

    adds link to logo

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -39,8 +39,10 @@ return (
     <header>
         <div className="mobile-wrapper">
             <div className="mobile-nav container mobile-nav-container">
-                <img id="mobile-logo" src={Logo} alt="mobile_company_logo" height="60" /> 
-                <button aria-label="open the menu" id="menu-button" className={`hamburger hamburger--spring ${isMobile ||navActive ? 'is-active' : ''}`} onClick={onClickHandler} type="button">
+                <img style={{position: 'relative', marginRight:'auto !important'}} id="mobile-logo" src={Logo} alt="mobile_company_logo" height="60" /> 
+                <Link style={{ position: 'absolute', left: 0, top: 0,  width: '100%', height: '100%',transform: 'translate(-50%)' }} to={'/'}/>
+
+                <button aria-label="open the menu" id="menu-button" className={`hamburger hamburger--spring ${isMobile && navActive ? 'is-active' : ''}`} onClick={onClickHandler} type="button">
                     <span aria-hidden="true" className="hamburger-box">
                         <span aria-hidden="true" className="hamburger-inner"></span>
                     </span>
@@ -53,7 +55,7 @@ return (
             {isMobile ?
             <div style={navActive  ? {width: '60%', position: 'fixed'} :  {width: 0}} className="navbar-menu">
                 <div id="side-menu" className="side-nav">
-                <img style={{position: 'relative', marginRight:'auto !important'}} src={Logo} alt="company logo" width="100px" / >                
+                <img style={{position: 'relative', marginRight:'auto !important'}} src={Logo} alt="company logo" width="100px" />                
                 <Link style={{ position: 'absolute', left: 0, top: 0,  width: '100%', height: '100%',transform: 'translate(-50%)' }} to={'/'}/>
                     <ul  id="on-top" className={`navbar-links ${!navActive ? 'clicked' : ''}`}>
                         {navLinks.map((navLink)=>{
