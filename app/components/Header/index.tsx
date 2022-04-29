df96d33a8fdb95923b187a6f9d63cd6bf66f66eb
Alexander M Long <alexanderlcodes@gmail.com>
2022-04-28 22:37:43 -0700

    adds mobile menu

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { Link, useLocation } from "remix";
+import useMedia from "../../helpers/useMedia";
 
 import Logo from "../../assets/images/logo.png";
 
@@ -6,15 +8,37 @@ import Logo from "../../assets/images/logo.png";
  * Get a link with the current locale parameter
  * @returns A localized <Link>
  */
+
+
 const Header = () => {
     const location = useLocation();     
     const navLinks = [{ name: 'Home', route: '/'},{ name: 'About', route: '/about'},{ name: 'Services', route: '/services'},{ name: 'Contact', route: '/contact'}]
+    const [navActive,setNavActive] = useState(false);
+
+    let isMobile: boolean | null = null;
+    if(typeof window !== 'undefined') {
+         isMobile = useMedia<boolean>(
+            // Media queries
+            ["(min-width: 1250px)", "(max-width: 1249px)"],
+            // Column counts (relates to above media queries by array index)
+            [false, true],
+    
+            false
+       
+          );
+    
+    }
+ 
+
+const onClickHandler = ()=> {
+    setNavActive(!navActive);
+}
 return (
     <header>
         <div className="mobile-wrapper">
             <div className="mobile-nav container mobile-nav-container">
                 <img id="mobile-logo" src={Logo} alt="mobile_company_logo" height="60" /> 
-                <button aria-label="open the menu" id="menu-button" className="hamburger hamburger--spring" onClick="toggleNav()" type="button">
+                <button aria-label="open the menu" id="menu-button" className={`hamburger hamburger--spring ${navActive || !isMobile  ? 'is-active' : ''}`} onClick={onClickHandler} type="button">
                     <span aria-hidden="true" className="hamburger-box">
                         <span aria-hidden="true" className="hamburger-inner"></span>
                     </span>
@@ -24,12 +48,12 @@ return (
 
 
         <nav>
-            <div className="navbar-menu">
+            <div style={navActive || !isMobile   ? {width: '60%'} :  {width: 0 }} className="navbar-menu">
                 <div id="side-menu" className="side-nav">
                     <img src={Logo} alt="company logo" width="100px" />
-                    <ul  id="on-top" className="navbar-links">
+                    <ul  id="on-top" className={`navbar-links ${!navActive ? 'clicked' : ''}`}>
                         {navLinks.map((navLink)=>{
-                            return <li key={navLink.name + navLink.route} className="nav-li"><Link className={location.pathname === navLink.route ? 'active-menu' : ''}  
+                            return <li style={navActive || !isMobile ? {opacity: 1, visibility: 'visible'} :  {opacity: 0, visibility: 'hidden'}} key={navLink.name + navLink.route} className="nav-li"><Link style={navActive || !isMobile   ? {opacity: 1, visibility: 'visible'} :  {opacity: 0, visibility: 'hidden'}} className={location.pathname === navLink.route ? 'active-menu' : ''}  
                             to={navLink.route}>{navLink.name}</Link></li>
 
 })}
@@ -44,3 +68,5 @@ return (
 };
 
 export default Header;
+
+
