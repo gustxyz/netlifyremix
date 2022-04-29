c43213a52996b4aa4ef421d1239b2551033f5084
Alexander M Long <alexanderlcodes@gmail.com>
2022-04-29 12:17:46 -0700

    add meta tags

--- app/root.tsx
+++ app/root.tsx
@@ -30,8 +30,33 @@ export const meta: MetaFunction = () => {
 const BaseTemplate: React.FC = ({ children }) => (
   <html lang="en">
     <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width,initial-scale=1" />
+   
+
+    <meta charSet="UTF-8"  />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <meta name="description" content="No page builders or WordPress - We offer 100% hand-coded websites with superior results starting at $150/mo." />
+    <meta name="keywords" content="small business web designer usa, small business web developer usa, web developer near me usa, affordable web design usa, affordable business website usa, 
+    web design agency usa, affordable web design agency for small businesses, custom web development usa, web design ageency, web design services usa, website designing compnay usa" />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="theme-color" content="#43b7ff" />
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <link rel="manifest" href="/site.webmanifest" />
+    <meta name="msapplication-TileColor" content="#da532c" />
+    <meta name="theme-color" content="#ffffff" />
+
+    <meta property="og:title" content="Small Business Web Design + Development"/>
+    <meta property="og:description" content="No page builders or WordPress - We offer 100% hand-coded websites with superior results starting at $150/mo."/>
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://www.alki.digital/"/>
+    <meta property="og:image" content="https://www.alki.digital/"/>
+    <meta property="og:image:secure_url" content="https://www.alki.digital/"/>
+
+    <link rel="canonical" href="https://www.alki.digital/" />
+
       <Meta />
       <Links />
     
@@ -39,6 +64,16 @@ const BaseTemplate: React.FC = ({ children }) => (
     <body>
       {children}
       <ScrollRestoration />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-226926212-1">
+    </script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-226926212-1');
+    </script>
+
       <Scripts />
       {process.env.NODE_ENV === "development" && <LiveReload />}
     </body>
